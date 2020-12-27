c4f9b9c40304bec80cda7b33fbdc5d930fc94d6c
smshahzaib <smshahzaibshah@gmail.com>
2020-12-27 17:13:56 +0500

    Sessional 2 and assignment 3 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useCallback, useState } from "react";
 
 function App() {
+  const [MPR, setMPR] = useState("");
+  const [date, setDate] = useState("");
+
+  const sendReq = () => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: '',
+    };
+    fetch("http://localhost:3000/doctor/delete/"+MPR+"/"+date, requestOptions)
+      .then((response) => {
+        alert("Mubrak ho gee data delete ho gya Successfully!");
+      }
+      ).catch(err=>{console.log(err)})
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="number"
+        placeholder="Enter MPR number"
+        onChange={(e) => setMPR(e.target.value)}
+      />
+      <br />
+      <input
+        type="string"
+        placeholder="Enter date"
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <br />
+      <input type="submit" value="delete" onClick={sendReq} />
     </div>
   );
 }
